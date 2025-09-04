81566260a79af4f97f0bf0a69431ddb5c97f1c83
kirito <kirito666codr@gmail.com>
2025-09-04 14:03:57 +0530

    Update AgencyCard component to replace images and text with 'Olivier Roeyaerts'; enhance layout by removing background colors and adding overflow handling for improved visual presentation.

--- src/components/page/Agency/AgencyCard.tsx
+++ src/components/page/Agency/AgencyCard.tsx
@@ -6,8 +6,8 @@ const AgencyCard = () => {
 
     <div  className=" h-[500px] md:h-[1000px] w-full sticky top-50 md:top-0 mx-auto mt-40 flex justify-center items-center z-10">
 
-      <div className="bg-red-900 h-full w-80 md:w-[600px] rounded-4xl z-10">
-        
+      <div className="h-full w-80 md:w-[600px] rounded-4xl z-10 overflow-hidden">
+        <img src="./images/Claire.jpg" className="h-full w-full object-cover" alt="" />
       </div>
 
     </div>
@@ -42,21 +42,22 @@ const AgencyCard = () => {
 
     <div className=" h-[500px] md:h-[1000px] w-full sticky top-50 md:top-0 mx-auto mt-40 flex justify-center items-center z-10">
     
-      <div className="bg-blue-900 h-full w-80 md:w-[600px] rounded-4xl z-10 relative"></div>
-
+      <div className="h-full w-80 md:w-[600px] rounded-4xl z-10 relative overflow-hidden">
+       <img src="./images/Olivier.jpg" className="h-full w-full object-cover" alt="" />
+      </div>
     </div>
 
     <div className="absolute w-full top-260 md:top-350 overflow-hidden z-0">
       <div className="scroll-to-left">
           <div className="flex gap-40">
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
           </div>
           <div className="flex gap-40">
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
           </div>
         </div>
       </div>
@@ -64,14 +65,14 @@ const AgencyCard = () => {
       <div className="absolute w-full top-300 md:top-440  left-0 overflow-hidden z-10 ">
         <div className="scroll-to-right ">
           <div className="flex gap-40 ">
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
           </div>
           <div className="flex gap-40">
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
-            <span className="uppercase text-lime-300 text-6xl md:text-9xl">claire robert</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
+            <span className="uppercase text-lime-300 text-6xl md:text-9xl">olivier roeyaerts</span>
           </div>
         </div>
       </div>
